e11211556e6e11ee1659a6f631827885adb488db
sorattomanuela <sorattomanuela@gmail.com>
2020-04-03 10:04:10 -0300

    adding new models

--- reddit/models.py
+++ reddit/models.py
@@ -7,11 +7,29 @@ from helpers.models import TimestampModel
 class Topic(TimestampModel):
     name = models.CharField(max_length=100, blank=True, default='')
     title = models.CharField(max_length=100, blank=True, default='')
-    author = models.ForeignKey('accounts.User', related_name='topic', on_delete=models.CASCADE) #related_name to specifies the name of reverse relation from the user back to model
+    author = models.ForeignKey('accounts.User', related_name='topics', on_delete=models.CASCADE) #related_name to specifies the name of reverse relation from the user back to model
     description = models.TextField(default='categories')
     urlname = models.SlugField(max_length=100, db_index=True, allow_unicode=False)
 
 
     class Meta:
         ordering = ['created_at']
+
+class Post(TimestampModel):
+    title = models.CharField(max_length=100, blank=True, default='')
+    content = models.TextField(default='categories')
+    author = models.ForeignKey('accounts.User', related_name='posts', on_delete=models.CASCADE)
+    topic = models.ForeignKey('reddit.Topic', related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_at', 'updated_at',]
+
+class Comment(TimestampModel):
+    content = models.TextField(default='categories')
+    author = models.ForeignKey('accounts.User', related_name='comments', on_delete=models.CASCADE)
+    topic = models.ForeignKey('reddit.Topic', related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey('reddit.Post', related_name='comments', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_at', 'updated_at',]
 
